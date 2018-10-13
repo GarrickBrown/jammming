0b5e581d26b294e3620b01f94f0cb3fc77848e7c
Garrick Brown <41980127+GarrickBrown@users.noreply.github.com>
2018-10-13 20:46:13 +0800

    Add basic search functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -56,6 +56,7 @@ class App extends Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -89,12 +90,16 @@ class App extends Component {
     console.log(trackURIs);
   }
 
+  search(searchTerm) {
+    console.log(searchTerm);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}
                            onAdd={this.addTrack}
